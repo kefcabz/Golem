8fc29ff9203893348d7d505ccec81406e131f262
Kef Caban <kefbball@gmail.com>
2025-11-04 14:01:10 -0500

    Change Speed of Oblisk

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -7,12 +7,15 @@ public class Patrol : MonoBehaviour
     public GameObject orePile;
     public Transform targetDirection;
     public GameObject projPrefab;
+    public float patrolSpeed = 20f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         golemPatrol = GetComponent<NavMeshAgent>();
         orePile = GameObject.FindGameObjectWithTag("Objective");
         golemPatrol.SetDestination(orePile.transform.position);
+        golemPatrol.speed = patrolSpeed;
     }
 
     // Update is called once per frame

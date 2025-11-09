66b5fcb8fb2157efe175b2c1229998eb132c3307
kefcabz <kefbball@gmail.com>
2025-11-08 22:49:53 -0500

    Added functionality to game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
-using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public enum GameState
 {
@@ -15,6 +18,17 @@ public class GameManager : MonoBehaviour
     public GameObject titleScreenPanel;
     public GameObject instructionsPanel;
     public GameObject gameHUDPanel;
+
+    public int oreMaxHealth = 100;
+    public int oreCurrentHealth;
+    public Slider oreHealthBar;
+    public GameObject oreObject;
+
+    public GameTimer gameTimer;
+
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI gameOverTimeText;
+
     public GameState currentState = GameState.Title;
     public static GameManager Instance { get; private set; }
 
@@ -33,6 +47,16 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         UpdateGameState(GameState.Title);
+
+        oreCurrentHealth = oreMaxHealth;
+        if (oreHealthBar != null)
+        {
+            oreHealthBar.maxValue = oreMaxHealth;
+            oreHealthBar.value = oreCurrentHealth;
+        }
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
     }
 
     public void UpdateGameState(GameState newState)
@@ -87,6 +111,9 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         UpdateGameState(GameState.Playing);
+
+        if (gameTimer != null)
+            gameTimer.StartTimer();
     }
 
     public void ShowInstructions()
@@ -97,5 +124,55 @@ public class GameManager : MonoBehaviour
     public void BackToTitle()
     {
         UpdateGameState(GameState.Title);
+    }
+
+    public void DamageOre(int amount)
+    {
+        oreCurrentHealth -= amount;
+        oreCurrentHealth = Mathf.Clamp(oreCurrentHealth, 0, oreMaxHealth);
+
+        if (oreHealthBar != null)
+            oreHealthBar.value = oreCurrentHealth;
+
+        if (oreCurrentHealth <= 0)
+            DestroyOre();
+    }
+
+    private void DestroyOre()
+    {
+        Debug.Log("Ore destroyed!");
+        if (oreObject != null)
+            Destroy(oreObject);
+
+        if (gameTimer != null)
+            gameTimer.StopTimer();
+
+        ShowGameOver();
+    }
+
+    private void ShowGameOver()
+    {
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+
+        if (gameOverTimeText != null && gameTimer != null)
+            gameOverTimeText.text = "Time Protected: " + gameTimer.GetElapsedTime().ToString("F2") + "s";
+
+        // Pause the game
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Resume time
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
     }
 }

670851d072904a479523602c270162b66a888f6a
kefcabz <kefbball@gmail.com>
2025-11-08 22:49:42 -0500

    Made more detailed time

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
-using TMPro; 
- 
+using TMPro;
+
 public class GameTimer : MonoBehaviour
 {
-    public TextMeshProUGUI gameTimerText; 
+    public TextMeshProUGUI gameTimerText;
+
+    private float elapsedTime = 0f;
+    private bool timerRunning = false;
 
     void Update()
     {
-        if (gameTimerText != null)
+        if (timerRunning)
         {
-            gameTimerText.text = "Time Protected: " + Time.time.ToString("F2"); // decimal places
+            elapsedTime += Time.deltaTime;
+
+            if (gameTimerText != null)
+                gameTimerText.text = "Time Protected: " + elapsedTime.ToString("F2") + "s";
         }
+    }
+    public void StartTimer()
+    {
+        timerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        timerRunning = false;
+    }
+    public void ResetTimer()
+    {
+        elapsedTime = 0f;
+        if (gameTimerText != null)
+            gameTimerText.text = "Time Protected: 0.00s";
+    }
+
+    public float GetElapsedTime()
+    {
+        return elapsedTime;
     }
 }
